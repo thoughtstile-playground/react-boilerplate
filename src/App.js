7bc10b50739d940502c3627200b1ee0f3983ee41
thoughtstile <thoughtstile@gmail.com>
2019-12-03 18:06:29 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='text-4xl font-bold text-center text-blue-500'>
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
